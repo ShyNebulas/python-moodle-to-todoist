f7e62cd35488fd00833b44abca558c65c59805bf
ShyNebulas <cameron352bell@gmail.com>
2023-09-23 16:47:31 +0100

    feat: filtered out headers and tasks

--- src/main.py
+++ src/main.py
@@ -1,14 +1,31 @@
 import os
 from dotenv import load_dotenv
+from bs4 import BeautifulSoup
+import re
 from todoist_api_python.api import TodoistAPI
 
 load_dotenv()
 
+blacklisted_terms = ["weekly", "support", "session", "submission", "slides", "zoom", "folder", "forum", "discussion", "solution"]
 with os.scandir("html") as directory:
     for entry in directory:
         if not entry.name.startswith('.') and entry.is_file():
             with open(entry.path, "r") as file:
-                print(entry.path)
+                soup = BeautifulSoup(file, "html.parser")
+                sections = soup.select(".content")
+                for section in sections: 
+                    section_name = section.select(".sectionname")
+                    if section_name: print(section_name[0].text)
+                    items = section.select(".instancename")
+                    for item in items:
+                        print(item.text)
+                       
+
+
+
+         
+          
+
 
 
 # todoist_api_key = os.getenv("TODOIST_API_KEY")
