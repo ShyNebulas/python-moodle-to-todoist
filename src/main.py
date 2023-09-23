fa95904d39528ae3f985c092c545732c91bfc9ca
ShyNebulas <cameron352bell@gmail.com>
2023-09-23 15:55:59 +0100

    feat: added file reading

--- src/main.py
+++ src/main.py
@@ -4,6 +4,13 @@ from todoist_api_python.api import TodoistAPI
 
 load_dotenv()
 
+with os.scandir("html") as directory:
+    for entry in directory:
+        if not entry.name.startswith('.') and entry.is_file():
+            with open(entry.path, "r") as file:
+                print(entry.path)
+
+
 # todoist_api_key = os.getenv("TODOIST_API_KEY")
 # api = TodoistAPI(todoist_api_key)
 
